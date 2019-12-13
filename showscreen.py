a744c6deded9bf7fb6085c212a8a72f7628abb1f
JerryALee <408362857@qq.com>
2019-12-13 14:06:24 +0800

    开始界面、作者、游戏说明

--- showscreen.py
+++ showscreen.py
@@ -4,9 +4,9 @@ import random
 
 import text
 
-def showAuthor(window):
-    window_size = window.get_size()
-    author_window = pygame.Surface(window_size)
+def showAuthor(screen):
+    screen_size = screen.get_size()
+    author_window = pygame.Surface(screen_size)
     author_window = author_window.convert()
     author_window.fill((178, 233, 238))
 
@@ -14,11 +14,11 @@ def showAuthor(window):
     (hmc, lcr, htq, wsn, jby, back, back1) = text.getAuthorText()
     author_font_size = hmc.get_size()
     back_font_size = back.get_size()
-    author_window.blit(hmc, ((window_size[0] - author_font_size[0])/2, 100))
-    author_window.blit(lcr, ((window_size[0] - author_font_size[0])/2, 150))
-    author_window.blit(htq, ((window_size[0] - author_font_size[0])/2, 200))
-    author_window.blit(wsn, ((window_size[0] - author_font_size[0])/2, 250))
-    author_window.blit(jby, ((window_size[0] - author_font_size[0])/2, 300))
+    author_window.blit(hmc, ((screen_size[0] - author_font_size[0])/2, 100))
+    author_window.blit(lcr, ((screen_size[0] - author_font_size[0])/2, 150))
+    author_window.blit(htq, ((screen_size[0] - author_font_size[0])/2, 200))
+    author_window.blit(wsn, ((screen_size[0] - author_font_size[0])/2, 250))
+    author_window.blit(jby, ((screen_size[0] - author_font_size[0])/2, 300))
     while True:
         buttons = pygame.mouse.get_pressed()
         x1, y1 = pygame.mouse.get_pos()
@@ -29,16 +29,16 @@ def showAuthor(window):
                 break
         else:
             author_window.blit(back, back_pos)
-        window.blit(author_window, (0, 0))
+        screen.blit(author_window, (0, 0))
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                print("退出游戏…")
+                print("Exiting...")
                 sys.exit()
 
-def showIntro(window):
-    window_size = window.get_size()
-    intro_window = pygame.Surface(window_size)
+def showIntro(screen):
+    screen_size = screen.get_size()
+    intro_window = pygame.Surface(screen_size)
     intro_window = intro_window.convert()
     intro_window.fill((178, 233, 238))
 
@@ -46,7 +46,7 @@ def showIntro(window):
     (intro, back, back1) = text.getIntroText()
     intro_font_size = intro.get_size()
     back_font_size = back.get_size()
-    intro_window.blit(intro, ((window_size[0] - intro_font_size[0])/2, 100))
+    intro_window.blit(intro, ((screen_size[0] - intro_font_size[0])/2, 100))
     while True:
         buttons = pygame.mouse.get_pressed()
         x1, y1 = pygame.mouse.get_pos()
@@ -57,19 +57,19 @@ def showIntro(window):
                 break
         else:
             intro_window.blit(back, back_pos)
-        window.blit(intro_window, (0, 0))
+        screen.blit(intro_window, (0, 0))
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                print("退出游戏…")
+                print("Exiting...")
                 sys.exit()
 
-def showGame(window):
+def showGame(screen):
     while True:
         background = pygame.image.load('./images/background.png').convert_alpha()
-        window.blit(background, (100, 100))
+        screen.blit(background, (100, 100))
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                print("退出游戏…")
+                print("Exiting...")
                 sys.exit()
--- start.py
+++ start.py
@@ -1,17 +1,17 @@
 import pygame
 import sys
 import random
-
 import text
 import showscreen
 
 pygame.init()
-window = pygame.display.set_mode((1024, 768), 32) # 窗口大小
-window_size = window.get_size()
+screen = pygame.display.set_mode((1024, 768)) # 窗口大小
 pygame.display.set_caption("Adaptation") # 窗口名，Adaptation
+pygame.display.set_icon(pygame.image.load("./images/Logo.ico")) # 图标
+screen_size = screen.get_size()
 clock = pygame.time.Clock()
-clock.tick(60)
-start_window = pygame.Surface(window_size)
+clock.tick(60) # FPS
+start_window = pygame.Surface(screen_size)
 start_window = start_window.convert()
 start_window.fill((178, 233, 238))
 
@@ -21,38 +21,37 @@ start_font_size = ksyx.get_size()
 while True:
     buttons = pygame.mouse.get_pressed()
     x1, y1 = pygame.mouse.get_pos()
-    if x1 >= (window_size[0] - start_font_size[0])/2 and x1 <= (window_size[0] + start_font_size[0])/2 \
+    if x1 >= (screen_size[0] - start_font_size[0])/2 and x1 <= (screen_size[0] + start_font_size[0])/2 \
         and y1 >= 400 and y1 <= 400 + start_font_size[1]:
-        start_window.blit(ksyx1, ((window_size[0] - start_font_size[0])/2, 400))
+        start_window.blit(ksyx1, ((screen_size[0] - start_font_size[0])/2, 400))
         if buttons[0]:
-            showscreen.showGame(window) # 进入游戏
-    elif x1 >= (window_size[0] - start_font_size[0])/2 and x1 <= (window_size[0] + start_font_size[0])/2 and \
+            showscreen.showGame(screen) # 进入游戏
+    elif x1 >= (screen_size[0] - start_font_size[0])/2 and x1 <= (screen_size[0] + start_font_size[0])/2 and \
         y1 >= 480 and y1 <= 480 + start_font_size[1]:
-        start_window.blit(yxsm1, ((window_size[0] - start_font_size[0])/2, 480))
+        start_window.blit(yxsm1, ((screen_size[0] - start_font_size[0])/2, 480))
         if buttons[0]:
-            showscreen.showIntro(window) # 进入游戏说明界面
-    elif x1 >= (window_size[0] - start_font_size[0])/2 and x1 <= (window_size[0] + start_font_size[0])/2 and \
+            showscreen.showIntro(screen) # 进入游戏说明界面
+    elif x1 >= (screen_size[0] - start_font_size[0])/2 and x1 <= (screen_size[0] + start_font_size[0])/2 and \
         y1 >= 560 and y1 <= 560 + start_font_size[1]:
-        start_window.blit(gyzz1, ((window_size[0] - start_font_size[0])/2, 560))
+        start_window.blit(gyzz1, ((screen_size[0] - start_font_size[0])/2, 560))
         if buttons[0]:
-            showscreen.showAuthor(window) # 进入作者界面
-    elif x1 >= (window_size[0] - start_font_size[0])/2 and x1 <= (window_size[0] + start_font_size[0])/2 and \
+            showscreen.showAuthor(screen) # 进入作者界面
+    elif x1 >= (screen_size[0] - start_font_size[0])/2 and x1 <= (screen_size[0] + start_font_size[0])/2 and \
         y1 >= 640 and y1 <= 640 + start_font_size[1]:
-        start_window.blit(tcyx1, ((window_size[0] - start_font_size[0])/2, 640))
+        start_window.blit(tcyx1, ((screen_size[0] - start_font_size[0])/2, 640))
         if buttons[0]:
             print("Exiting...")
             sys.exit() # 退出游戏
     else:
-        start_window.blit(title, (((window_size[0] - title.get_size()[0])/2, 50)))
-        start_window.blit(ksyx, ((window_size[0] - start_font_size[0])/2, 400))
-        start_window.blit(yxsm, ((window_size[0] - start_font_size[0])/2, 480))
-        start_window.blit(gyzz, ((window_size[0] - start_font_size[0])/2, 560))
-        start_window.blit(tcyx, ((window_size[0] - start_font_size[0])/2, 640))
+        start_window.blit(title, (((screen_size[0] - title.get_size()[0])/2, 50)))
+        start_window.blit(ksyx, ((screen_size[0] - start_font_size[0])/2, 400))
+        start_window.blit(yxsm, ((screen_size[0] - start_font_size[0])/2, 480))
+        start_window.blit(gyzz, ((screen_size[0] - start_font_size[0])/2, 560))
+        start_window.blit(tcyx, ((screen_size[0] - start_font_size[0])/2, 640))
     
-    window.blit(start_window, (0, 0))
-    pygame.display.update()
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             print("Exiting...")
             sys.exit()
+    screen.blit(start_window, (0, 0))
+    pygame.display.update()
