03702170b93a35f93674ed17737a1f4741083ba4
Billythecrescent <13550378056@163.com>
2019-12-14 16:51:07 +0800

    Biofilm generation, biofilm movement, biofilm color changing, setting

--- settings.py
+++ settings.py
@@ -8,3 +8,18 @@ class Settings(object):
         self.screen_height = 800
         self.bg_color = (230,230,230)
         self.force_const = 0.15
+
+        #ball settings
+        self.ball_size = (40,40)
+        self.ball_iniPos = (492,364)
+        self.ball_iniCol = "red"
+        self.ball_left = 0 # 0 or -1
+        self.ball_right = 0 # 0 or 1
+        self.ball_speed = 0
+
+        #Biofilm settings
+        self.film_size = (4,1024)
+        self.film_iniPos = 1024
+        self.color_order = ["red", "yellow", "green", "blue", "purple"]
+        self.film_speed = 1
+        self.film_lambda = 1/6
